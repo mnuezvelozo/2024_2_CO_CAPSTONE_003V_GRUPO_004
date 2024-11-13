002254dc23a10d6674697ea108e71d91771aa5ae
JoacoRdrgz <joa.flores@duocuc.cl>
2024-11-13 16:38:33 -0300

    Editar comentario de avance

--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Avance Pagina 13-11-2024/Modulo_Flota/conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion = mysqli_connect("localhost", "root", "", "modulo_flota");
-date_default_timezone_set('America/Santiago');
--- Fase 2/Evidencias Proyecto/Evidencias de sistema/Avance Pagina 13-11-2024/Modulo_Flota/login/cerrar_sesion.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destruye todas las variables de sesión
-header("Location: ../login/login.php"); // Redirige al login después de cerrar sesión
-exit();
-?>
